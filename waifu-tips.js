e49ceb227cc79b7fb449535e4425485c82944ba9
LianYiMing <AGZX_lym@163.com>
2020-10-08 11:45:04 +0800

    优化看伴娘

--- waifu-tips.js
+++ waifu-tips.js
@@ -26,11 +26,12 @@ function loadWidget(config) {
 				<span class="fa fa-lg fa-user-circle"></span>
 				<span class="fa fa-lg fa-street-view"></span>
 				<span class="fa fa-lg fa-camera-retro"></span>
-				<span class="fa fa-lg fa-info-circle"></span>
+			
 				<span class="fa fa-lg fa-times"></span>
 			</div>
 		</div>`);
 	// https://stackoverflow.com/questions/24148403/trigger-css-transition-on-appended-element
+		//<span class="fa fa-lg fa-info-circle"></span>
 	setTimeout(() => {
 		document.getElementById("waifu").style.bottom = 0;
 	}, 0);
